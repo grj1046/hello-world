67ae9d25d9747b4386ea5cacaafa3db83b1e3f44
grj1046 <guorenjun@outlook.com>
2016-05-18 18:53:51 +0800

    angular-translate example

---
+++ src/1.angularjs-translate/README.md
@@ -0,0 +1,7 @@
+# this example use angular 1.5.5 and angular-translate 2.9.0.1
+
+You must run the site as http protocol.
+
+https://angular-translate.github.io
+
+https://angular-translate.github.io/docs/#/guide/13_custom-loaders
---
+++ src/1.angularjs-translate/scripts/index.js
@@ -0,0 +1,35 @@
+﻿'use strict';
+
+var app = angular.module('App', ['pascalprecht.translate']);
+app.config(function ($translateProvider) {
+    $translateProvider.useLoader("asyncLoader");
+    $translateProvider.preferredLanguage('en');
+    $translateProvider.useSanitizeValueStrategy(null);////'sanitize'
+}).factory('asyncLoader', function ($q, $http) {
+    return function (operation) {
+        var deferred = $q.defer();
+
+        //if (operation.key === 'en') {
+        //    $http.get('lang/en.json').then(function (res) {
+        //        deferred.resolve(res.data);
+        //    })
+        //} else if (operation.key === 'cn') {
+        //    $http.get('lang/cn.json').then(function (res) {
+        //        deferred.resolve(res.data)
+        //    })
+        //}
+
+        $http.get('lang/' + operation.key + '.json').then(function (res) {
+            deferred.resolve(res.data);
+        });
+        return deferred.promise;
+    }
+}).controller("mainCtrl", function ($scope, $http, $translate) {
+    $http.get('lang/mapping.json').then(function (res) {
+        //suppose res.statusCode = 200
+        $scope.data = res.data;
+    });
+    $scope.changeLanguage = function (langKey) {
+        $translate.use(langKey);
+    }
+});
